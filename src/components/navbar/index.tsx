8067e75c6bcff916f340626609a2fba92c243b7f
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2024-01-09 16:06:46 +0530

    responsive navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
    
     return (
         <>
-            <div className="navbar flex justify-between  items-center w-full  px-10 py-4 " >
-                <div className="logo text-2xl font-semibold select-none">
+            <div className="navbar flex justify-between items-center w-full px-4 sm:px-10 py-8 sm:py-4 " >
+                <div className="logo text-xl sm:text-2xl font-semibold select-none">
                     Amante-Tassels
                 </div>
-                <div className="flex text-lg w-[30%] justify-between">
+                <div className="hidden sm:flex text-lg w-[30%] justify-between">
                     <Link href="" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white hover:font-medium ">Home</Link>
                     <Link href="/products" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white  hover:font-medium">Products</Link>
                     <Link href="/" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white  hover:font-medium">About</Link>
@@ -18,12 +18,12 @@ export const Navbar = () => {
                     <Link href="/" className="border-b-2 border-transparent hover:border-b-black  hover:dark:border-b-white   hover:font-medium">FAQ</Link>
                 </div>
                 <div className="flex items-center gap-2">
-                    <Link href="/login" className="flex items-center gap-2 p-2 rounded-3xl bg-white dark:text-black dark:border-gray-700 shadow hover:opacity-80 ">
-                        <span><CgProfile size={25}/></span>
+                    <Link href="/login" className="flex items-center sm:text-xl  gap-2 p-2 rounded-3xl bg-white dark:text-black dark:border-gray-700 shadow hover:opacity-80 ">
+                        <span><CgProfile/></span>
                         <span>Login</span>
                     </Link>
-                    <Link href="/login" className="flex items-center gap-2 p-2 rounded-3xl bg-white dark:text-black  dark:border-gray-700 shadow hover:opacity-80">
-                        <span><MdOutlineShoppingCart size={25}/></span>
+                    <Link href="/login" className="flex items-center sm:text-xl gap-2 p-2 rounded-3xl bg-white dark:text-black  dark:border-gray-700 shadow hover:opacity-80">
+                        <span><MdOutlineShoppingCart/></span>
                         <span>Cart</span>
                     </Link>
                 </div>
