3c210526760349f64a7b424776781dd1507e3b17
Taranjeet Singh <taranjeets120@gmail.com>
2024-01-16 09:03:23 -0800

    issue login and signup solved

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar, Footer } from "@/components"
+const Products = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Cart</h1>
+            <Footer />
+        </>
+    )
+}
+export default Products
