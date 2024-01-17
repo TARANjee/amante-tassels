82ce9e0f27af62b36def5822c04d4e6f9b43e78c
Taranjeet Singh <taranjeets120@gmail.com>
2024-01-17 10:34:56 -0800

    solve some issue

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,19 +6,18 @@ export const Navbar = () => {
 
     return (
         <>
-            <div className="navbar flex justify-between items-center w-full px-4 sm:px-10 py-8 sm:py-4 shadow-sm " >
+            <div className="navbar flex justify-between items-center w-full px-4 sm:px-10 py-8 sm:py-4 " >
                 <div className="logo text-xl sm:text-2xl font-semibold select-none">
                     Amante-Tassels
                 </div>
                 <div className="hidden sm:flex text-lg w-[30%] justify-between">
-                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black  hover:font-medium ">Home</Link>
-                    <Link href="/products" className="border-b-2 border-transparent hover:border-b-black   hover:font-medium">Products</Link>
-                    <Link href="/about" className="border-b-2 border-transparent hover:border-b-black hover:font-medium">About</Link>
-                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black    hover:font-medium">Contact</Link>
-                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black     hover:font-medium">FAQ</Link>
+                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white hover:font-medium ">Home</Link>
+                    <Link href="/products" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white  hover:font-medium">Products</Link>
+                    <Link href="/about" className="border-b-2 border-transparent hover:border-b-black hover:dark:border-b-white  hover:font-medium">About</Link>
+                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black  hover:dark:border-b-white  hover:font-medium">Contact</Link>
+                    <Link href="/" className="border-b-2 border-transparent hover:border-b-black  hover:dark:border-b-white   hover:font-medium">FAQ</Link>
                 </div>
                 <div className="flex items-center gap-2">
-
                     <Link href="/login" className="flex items-center sm:text-xl  gap-2 p-2 rounded-3xl bg-white dark:text-black dark:border-gray-700 shadow hover:opacity-80 ">
                         <span><CgProfile /></span>
                         <span>Login</span>
@@ -28,15 +27,9 @@ export const Navbar = () => {
                             <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-4 -end-3 dark:border-gray-900">2</div>
 
                         </span>
-
-                    <Link href="/login" className="flex items-center sm:text-xl  gap-2 p-2 rounded-3xl bg-white  shadow hover:opacity-80 ">
-                        <span><CgProfile/></span>
-                        <span>Login</span>
-                    </Link>
-                    <Link href="/cart" className="flex items-center sm:text-xl gap-2 p-2 rounded-3xl bg-white shadow hover:opacity-80">
-                        <span><MdOutlineShoppingCart/></span>
-
                         <span>Cart</span>
+
+
 
                     </Link>
                 </div>
